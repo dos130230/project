debf904eda267474f3cdd01526d6706d0916c539
dos130230 <dostonbekoktamov765@gmail.com>
2022-02-27 02:32:23 +0500

    structurlar ozgardi

--- src/controller/messages.controller.js
+++ src/controller/messages.controller.js
@@ -8,18 +8,18 @@ class MessageController {
         const { toSend } = req.body
         // console.log(toSend)
         let messages = await req.fetch(`
-                    select 
-                        m.message_id,
-                        u.user_id,
-                        m.send_message,
-                        m.file_type,
-                        u.fullname,
-                        u.username,
-                        u.avatar_img
-                    from messages as m
-                    left join users as u on m.send_userId = u.user_id
-                    where (m.send_userId = $1 and m.received_userId = $2) or  (m.send_userId = $2 and m.received_userId = $1)
-                    order by m.message_id 
+             select 
+                 m.message_id,
+                 u.user_id,
+                 m.send_message,
+                 m.file_type,
+                 u.fullname,
+                 u.username,
+                 u.avatar_img
+             from messages as m
+             left join users as u on m.send_userId = u.user_id
+             where (m.send_userId = $1 and m.received_userId = $2) or  (m.send_userId = $2 and m.received_userId = $1)
+             order by m.message_id 
         `,req.user.user_id, +toSend)
         res.send(messages)
     }
